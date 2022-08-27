68f80dea7e18a0e1b8870dd07447111642fd4094
faishalmakarim <kamalifaishal1@gmail.com>
2022-08-27 16:51:32 +0700

    (task) : Adding exercise files

---
+++ 4_Introduction-to-Algorithm-and-Golang/praktikum/problem2.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var i int
+
+	fmt.Printf("Input : ")
+	fmt.Scanf("%d", &i)
+
+	if i%2 == 0 {
+		fmt.Print("lampu menyala")
+	} else {
+		fmt.Print("lampu mati")
+	}
+}
