4b0254f0e1402ada10bccba5fa93c6b8450971d4
faishalmakarim <kamalifaishal1@gmail.com>
2022-09-02 16:00:29 +0700

    (task) : Adding exercise files.

---
+++ 6_Data-Structure/praktikum/problem2.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func munculSekali(angka string) []int {
+	var result []int
+	var freq map[string]int = map[string]int{}
+
+	for _, num := range angka {
+		freq[string(num)]++
+	}
+	for key, freq := range freq {
+		if freq == 1 {
+			res, _ := strconv.Atoi(key)
+			result = append(result, res)
+		}
+	}
+	return result
+}
+
+func main() {
+	fmt.Println(munculSekali("1234123"))
+	fmt.Println(munculSekali("76523752"))
+	fmt.Println(munculSekali("12345"))
+	fmt.Println(munculSekali("1122334455"))
+	fmt.Println(munculSekali("0872504"))
+}
---
+++ 6_Data-Structure/praktikum/problem3.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func PairSum(arr []int, target int) []int {
+	var result []int = []int{}
+
+	var storage map[int]int = map[int]int{}
+
+	for index, b := range arr {
+		var a int = target - b
+
+		_, exist := storage[a]
+		if exist {
+			result = append(result, storage[a], index)
+			break
+		}
+		storage[b] = index
+	}
+
+	return result
+}
+
+func main() {
+	fmt.Println(PairSum([]int{1, 2, 3, 4, 6}, 6))
+	fmt.Println(PairSum([]int{2, 5, 9, 11}, 11))
+	fmt.Println(PairSum([]int{1, 3, 5, 7}, 12))
+	fmt.Println(PairSum([]int{1, 4, 6, 8}, 10))
+	fmt.Println(PairSum([]int{1, 5, 6, 7}, 6))
+}
